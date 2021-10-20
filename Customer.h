90f42b80c39e9082b1fc45aa9531c2cfa00a1a47
Haziq Hapiz <zyqq.dev@gmail.com>
2021-10-20 21:40:40 +0800

    create class

---
+++ Customer.h
@@ -0,0 +1,15 @@
+#ifndef CUSTOMER_H
+#define CUSTOMER_H
+
+#include <string>
+using namespace std;
+
+#pragma once
+
+class Customer
+{
+private:
+	int customerId;
+};
+
+#endif
---
+++ Rider.cpp
@@ -0,0 +1,1 @@
+#include "Rider.h"
